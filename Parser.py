55b8baf97e55b867af682b313f1338b28b8e17d5
Ekaterina Kutovaia <80972558+KateK1@users.noreply.github.com>
2022-10-02 18:12:51 +0200

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -12,26 +12,28 @@ sh['I1']= 'FinY'
 
 
 for row in range(2, sh.max_row):
-    artist = str(sh.cell(row=row, column=4).value)
+    artist = (str(sh.cell(row=row, column=4).value)).replace(' ', '_')
     print(artist)
-    ar = artist.replace(' ', '_')
-    req = requests.get('https://ru.wikipedia.org/wiki/'+ ar)
+    req = requests.get('https://ru.wikipedia.org/wiki/'+ artist)
     if req.status_code != 404:
-        req1 = urllib.request.urlopen('https://ru.wikipedia.org/wiki/' + ar)
+        req1 = urllib.request.urlopen('https://ru.wikipedia.org/wiki/' + artist)
         soup = BeautifulSoup(req1, features="lxml")
         for table in soup.find_all('table', attrs = {"class": "infobox"}):
             for r in table.find_all('tr'):
+                # find country for each artist
                 if r.text.find("Страна") >= 0:
                     for c in r.find_all('td'):
                         t = sh.cell(row=row, column=7)
                         country = str(c.text).strip().split('[')
                         print(str(country[0]))
                         t.value = str(country[0])
+                # find year when artist started career
                 if r.text.find('Год') >= 0:
                     for y in r.find_all('td'):
                         t = sh.cell(row=row, column=8)
                         b = sh.cell(row=row, column=9)
                         year = y.text.strip()
+                        # for artists who still active 'н' stands for 'Until now'
                         if 'н' in year:
                             year = str(year[0:5]+'-2021')
                         year = str(year).strip().split('[')
